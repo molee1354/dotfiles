a931349bf9df39de3d44d109fc471134d409e097
Mokin Lee <102238428+molee1354@users.noreply.github.com>
2024-05-31 14:23:07 +0900

    add:
    added qol stuff for zsh prompt and terminal stuff

--- .config/shell/main.sh
+++ .config/shell/main.sh
@@ -1,4 +1,9 @@
-# sourcing the files
 
+# setting zsh prompt for distrobox containers
+if [[ -n ${CONTAINER_ID} ]]; then
+    export PS1="%B%F{blue}[${CONTAINER_ID}]%f%b ${PS1}"
+fi
+
+# sourcing the files
 source ~/.config/shell/alias.sh
 source ~/.config/shell/functions.sh
